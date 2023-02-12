7a8f9745bd7acc292dbaf0e0723359649ef0eb39
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2023-02-12 23:34:45 +0300

    Readme updated

--- src/main/kotlin/KruskalMST.kt
+++ src/main/kotlin/KruskalMST.kt
@@ -1,45 +1,49 @@
 import java.util.*
-import kotlin.math.sqrt
 
-//fun main() {
-//    val sc = Scanner(System.`in`)
-//    val n = sc.nextInt()
-//    val m = sc.nextInt()
-//    val graph = Graph(n)
-//    for (i in 0 until m) {
-//        val a = sc.nextInt() - 1
-//        val b = sc.nextInt() - 1
-//        val w = sc.nextInt()
-//        graph.edges += Graph.Edge(a, b, w)
-//    }
-//    val res = KruskalMST(graph).solve()
-//    println(res)
-//}
+fun main() {
+//    val graph = readGraphUsingScanner()
+    val graph = readGraphUsingReadLine()
+    val res = KruskalMST(graph).solve()
+    println(res)
+}
+
+fun readGraphUsingScanner(): Graph<Int> {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val m = sc.nextInt()
+    val graph = Graph<Int>(n)
+    for (i in 0 until m) {
+        val a = sc.nextInt() - 1
+        val b = sc.nextInt() - 1
+        val w = sc.nextInt()
+        graph.edges += Graph.Edge(a, b, w)
+    }
+    return graph
+}
 
 fun readLineOfInts() = readLine()!!.split(" ").map(String::toInt)
 
-fun main() {
+fun readGraphUsingReadLine(): Graph<Int> {
     val (n, m) = readLineOfInts()
     val graph = Graph<Int>(n)
     for (i in 0 until m) {
         val (a, b, w) = readLineOfInts()
         graph.edges += Graph.Edge(a - 1, b - 1, w)
     }
-    val res = KruskalMST(graph).solve()
-    println(res)
+    return graph
 }
 
 class DSU(private val parent: IntArray, private val rank: IntArray) {
     constructor(n: Int) : this(IntArray(n) { it }, IntArray(n))
 
-    fun findSet(v: Int): Int = naiveFindSet(v)
+    fun findSet(v: Int): Int = findSetWithPathCompression(v)
 
     private fun naiveFindSet(v: Int) = if (v == parent[v]) v else findSet(parent[v])
 
     private fun findSetWithPathCompression(v: Int) =
         if (v == parent[v]) v else findSet(parent[v]).also { parent[v] = it }
 
-    fun unionSets(x: Int, y: Int) = naiveUnionSets(x, y)
+    fun unionSets(x: Int, y: Int) = unionSetsWithRanking(x, y)
 
     private fun naiveUnionSets(x: Int, y: Int) {
         val a = findSet(x)
@@ -73,8 +77,14 @@ class Graph<T>(val vertex_count: Int) {
 class KruskalMST(private val graph: Graph<Int>) {
     fun solve(): Long {
         graph.edges.sortBy { it.w }
-        var ans: Long = 0
         val dsu = DSU(graph.vertex_count)
+//        return graph.edges.sumOf { (a, b, w) ->
+//            if (dsu.findSet(a) != dsu.findSet(b)) {
+//                dsu.unionSets(a, b)
+//                w.toLong()
+//            } else 0L
+//        }
+        var ans: Long = 0
         for ((a, b, w) in graph.edges) {
             if (dsu.findSet(a) != dsu.findSet(b)) {
                 ans += w
