c196e6af1bbd6cb5fdafd2074d6d0c563d36799b
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2023-02-12 18:53:18 +0300

    Kruskal code reorganised

--- src/main/kotlin/KruskalMST.kt
+++ src/main/kotlin/KruskalMST.kt
@@ -1,7 +1,29 @@
 import java.util.*
 
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val n = sc.nextInt()
+//    val m = sc.nextInt()
+//    val graph = Graph(n)
+//    for (i in 0 until m) {
+//        val a = sc.nextInt() - 1
+//        val b = sc.nextInt() - 1
+//        val w = sc.nextInt()
+//        graph.edges += Graph.Edge(a, b, w)
+//    }
+//    val res = KruskalMST(graph).solve()
+//    println(res)
+//}
+
 fun main() {
-    KruskalMST().solve()
+    val (n, m) = readLine()!!.split(" ").map(String::toInt)
+    val graph = Graph(n)
+    for (i in 0 until m) {
+        val (a, b, w) = readLine()!!.split(" ").map(String::toInt)
+        graph.edges += Graph.Edge(a - 1, b - 1, w)
+    }
+    val res = KruskalMST(graph).solve()
+    println(res)
 }
 
 class DSU(private val parent: IntArray, private val rank: IntArray) {
@@ -25,33 +47,23 @@ class DSU(private val parent: IntArray, private val rank: IntArray) {
     }
 }
 
-class KruskalMST {
-    private val graph = mutableListOf<Triple>()
+class Graph(val vertex_count: Int) {
+    val edges = mutableListOf<Edge>()
 
-    fun solve() {
-        val sc = Scanner(System.`in`)
-        val n = sc.nextInt()
-        val m = sc.nextInt()
-        for (i in 0 until m) {
-            val a = sc.nextInt() - 1
-            val b = sc.nextInt() - 1
-            val c = sc.nextInt()
-            graph.add(Triple(c, a, b))
-        }
-        graph.sortBy { it.w }
+    data class Edge(val a: Int, val b: Int, val w: Int)
+}
+
+class KruskalMST(private val graph: Graph) {
+    fun solve(): Long {
+        graph.edges.sortBy { it.w }
         var ans: Long = 0
-        val dsu = DSU(n)
-        for (triple in graph) {
-            val w = triple.w
-            val b = triple.a
-            val c = triple.b
-            if (dsu.findSet(b) != dsu.findSet(c)) {
+        val dsu = DSU(graph.vertex_count)
+        for ((a, b, w) in graph.edges) {
+            if (dsu.findSet(a) != dsu.findSet(b)) {
                 ans += w
-                dsu.unionSets(b, c)
+                dsu.unionSets(a, b)
             }
         }
-        println(ans)
+        return ans
     }
-
-    class Triple(var w: Int, var a: Int, var b: Int)
 }
